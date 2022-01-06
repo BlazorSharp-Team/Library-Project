8277c64516f96385b23c4c632eb393df5aeb411a
FEJLESZTO001\user <fiedler.norbert@cts.hu>
2022-01-06 16:43:47 +0100

    További DBContext módosítás

--- Library Project/Library Project/Program.cs
+++ Library Project/Library Project/Program.cs
@@ -1,6 +1,7 @@
 using Library_Project.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +10,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<Books>();
-builder.Configuration.AddJsonFile("appsettings.json", optional: false,reloadOnChange:true);
-builder.Services.AddDbContext<Books>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Configuration.AddJsonFile("appsettings.json", optional: false,reloadOnChange:true);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
