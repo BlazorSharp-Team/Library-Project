f2680ee6294ac148774e5d22a8103dd7c63dc19a
BuildTools <unconfigured@null.spigotmc.org>
2022-01-10 19:57:45 +0100

    Interface, Controller hozzáadva és DBcontext frissítve. A Modellek alapján fel vannak építve ami a Datas mappa alatt találhatóak.

--- Library Project/Library Project/Data/AppDbContext.cs
+++ Library Project/Library Project/Data/AppDbContext.cs
@@ -2,8 +2,12 @@
 
 namespace Library_Project.Data
 {
-    public class AppDbContext : DbContext
+    public partial class AppDbContext : DbContext
     {
+
+        public AppDbContext()
+        {
+        }
 
         public AppDbContext(DbContextOptions <AppDbContext> options)
             :base (options)
@@ -12,5 +16,34 @@ namespace Library_Project.Data
         }
 
         public DbSet<Books> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Books>(entity =>
+            {
+                entity.ToTable("Books");
+                entity.Property(e => e.Id).HasColumnName("Userid");
+                entity.Property(e => e.Title)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+                entity.Property(e => e.isbnNumber)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+                entity.Property(e => e.Author)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+                entity.Property(e => e.Publisher)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+                entity.Property(e => e.Category)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+                entity.Property(e => e.PublishDate);
+                entity.Property(e => e.Quantity)
+                    .HasMaxLength(20);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
--- Library Project/Library Project/Program.cs
+++ Library Project/Library Project/Program.cs
@@ -1,6 +1,9 @@
 using Library_Project.Data;
+using Library_Project.Interfaces;
+using Library_Project.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,8 +13,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<Books>();
-//builder.Configuration.AddJsonFile("appsettings.json", optional: false,reloadOnChange:true);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<IBooks, BookManager>();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -28,7 +32,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.MapRazorPages();
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
